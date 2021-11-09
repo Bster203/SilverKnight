c60626e77275f8cc0f7bd5ad22c1efad54ea4f8e
Bster86 <86089590+Bster203@users.noreply.github.com>
2021-11-09 14:07:25 -0500

    basic animations

---
+++ SilverKnight/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+            transform.localScale = new Vector2(-1.5f, 1.5f);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(5, rb.velocity.y);
+            transform.localScale = new Vector2(1.5f, 1.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
+        }
+    }
+}
