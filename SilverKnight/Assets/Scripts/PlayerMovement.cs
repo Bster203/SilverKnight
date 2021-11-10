44f8d6159d3ce77a7460e1ca3ed22fad78041259
Bster86 <86089590+Bster203@users.noreply.github.com>
2021-11-10 01:47:09 -0500

    finished animations

--- SilverKnight/Assets/Scripts/PlayerMovement.cs
+++ SilverKnight/Assets/Scripts/PlayerMovement.cs
@@ -4,32 +4,78 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public Animator anim; //connects the player to the animator
+    private Rigidbody2D rb;
+    private Animator anim; 
+    private enum State {idle, running, jumping, falling}
+    private State state = State.idle;
+    private Collider2D coll;
+    [SerializeField] private LayerMask ground;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+    }
 
     private void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow)) //If the left arrow key is pressed and the right arrow key is NOT pressed
+
+        float hDirection = Input.GetAxis("Horizontal");
+
+        if(hDirection < 0) 
         {
-            rb.velocity = new Vector2(-5, rb.velocity.y); //the player will move left at a value of -5
+            rb.velocity = new Vector2(-8, rb.velocity.y); //the player will move left at a speed of -8
             transform.localScale = new Vector2(-1.5f, 1.5f); //the player will face left at a value of -1.5 with a height of 1.5
-            anim.SetBool("running", true); //the player will run left
         }
 
-       else if (Input.GetKey(KeyCode.RightArrow)) //If the right arrow is pressed and the left arrow key is NOT pressed
+       else if (hDirection > 0)
         {
-            rb.velocity = new Vector2(5, rb.velocity.y); //the player will move right at a value of 5
+            rb.velocity = new Vector2(8, rb.velocity.y); //the player will move right at a speed of 8
             transform.localScale = new Vector2(1.5f, 1.5f); //the player will face right at a value of 1.5 with a height of 1.5
-            anim.SetBool("running", true); //the player will run right
         }
         else
         {
-            anim.SetBool("running", false); //if the left arrow key is NOT pressed and the right arrow key is NOT pressed, the player will idle
+           
         }
 
-        if (Input.GetKeyDown(KeyCode.Space)) //if the space arrow is pressed
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground)) //if the space arrow is pressed
         {
-            rb.velocity = new Vector2(rb.velocity.x, 10f); //the player will jump with a value of 10
+            rb.velocity = new Vector2(rb.velocity.x, 20f); //the player will jump with a value of 10
+            state = State.jumping;
         }
+
+        VelocityState();
+        anim.SetInteger("state", (int)state);
+    }
+
+    private void VelocityState()
+    {
+        if(state == State.jumping)
+        {
+            if(rb.velocity.y < .1f)
+            {
+                state = State.falling;
+            }
+        }
+        else if(state == State.falling)
+        {
+            if (coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+
+        else if(Mathf.Abs(rb.velocity.x) > 2f) //if the velocity inside the x-axis is greater than 0.1
+        {
+            //Moving
+            state = State.running;
+        }
+
+        else
+        {
+            state = State.idle;
+        }
+        
     }
 }
